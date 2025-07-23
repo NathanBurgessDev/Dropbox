1926f46835e8d1fedaeaeb50a56f6579b54cff13
Nathan Burgess <nathan.burgessdev@gmail.com>
2025-07-23 03:49:53 +0100

    File moving broken

--- client/client.py
+++ client/client.py
@@ -49,6 +49,18 @@ class MyEventHandler(FileSystemEventHandler):
         return super().on_deleted(event)
 
     # ignore DirModifiedEvent - can be fired with non changes
+    # Fun little race condition here:
+    # As we are uploading the file in a chunked format we do a few things
+    # 1. The file is opened *but not read into memory* this allows us to upload larger files
+    # 2. When making our post request the file size is calculated and the expected size is set
+    # 3. As the file is streamed - If the file is edited and saved again between the file size calculated and when the file is streamed - the size of the file has changed
+    # we will get an "h11._util.LocalProtcolError: Too much / Too Little data for declared Content-Length"
+    # Our http request expects a smaller file than has been provided by the stream
+    # How do we solve this???
+    # 1. Ignore support for larger files and load the file into memory
+    # 2. Catch the error and keep trying until it works
+    # 3. Create a copy of the file elsewhere and upload that - time + storage intensive
+    # 4. Lock the file - reaaaaaly janky and likely to be very painful
     def on_modified(self, event):
         if not (event.is_directory):
             print("FILE MODIFIED ")
@@ -59,6 +71,7 @@ class MyEventHandler(FileSystemEventHandler):
                 files = {"file": open(event.src_path, "rb")}
             except:
                 print("File failed to open")
+                return
             print(files)
             r = httpx.post(
                 "http://localhost:8000/uploadfile", files=files, data=dataPath
@@ -80,8 +93,11 @@ if __name__ == "__main__":
     observer.start()
 
     try:
-        while True:
+        while observer.is_alive():
             time.sleep(1)
-    finally:
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received.")
         observer.stop()
-        observer.join()
+    observer.join()
+
+    exit(0)
